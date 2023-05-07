ff1b8e19a90d13e7d298ccbd62d2f08f24c08383
Aarland <adrian.aarland@gmail.com>
2023-05-07 13:17:11 -0500

    added file read and stdin

--- grep_lite/src/main.rs
+++ grep_lite/src/main.rs
@@ -1,18 +1,48 @@
-use regex::Regex;    // <1>
+use std::{fs::File, io::prelude::*, io::BufReader};
+
+use clap::{App, Arg};
+use regex::Regex; // <1>
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line in reader.lines() {
+        let line = line.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", &line),
+            None => (),
+        }
+    }
+}
 
 fn main() {
-  let re = Regex::new("picture").unwrap();    // <2>
+    let args = App::new("grep-lite")
+        .version("1.0")
+        .about("Searches for patterns in files")
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
 
-  let quote = "Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?";
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap(); // <2>
+                                           //
+    let input = args.value_of("input").unwrap();
 
-  for line in quote.lines() {
-    let contains_substring = re.find(line);
-    match contains_substring {    // <3>
-
-        Some(_) => println!("{}", line),    // <4>
-        None => (),    // <5>
+    if input == "-" {
+        let stdin = std::io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
-  }
 }
